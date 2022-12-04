908df150a38ed88f6c01c8803a5ede5429b73f70
Roman <bashkatovr@mail.ru>
2022-12-04 21:30:49 +0300

    Коммит 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = arrayRandom(30);
+        // Найти сумму расходов
+        int sum = 0;
+        for (int number : array) {
+            sum += number;
+        }
+        System.out.println(sum);
+        // Найти максимальный и минимальный расход
+        int min = array[0];
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        System.out.println(" Минимальное значение = " + min + "; Максимальное зщанчение = " + max);
+        {
+            public static int[] arrayRandom(int length){
+            Random random = new Random();
+            int[] array = new int[length];
+            for (int i = 0; i < length; i++) {
+                array[i] = random.nextInt(100_000) + 100_000;
+            }
+            return array;
+        }
+        }
     }
 }
+
